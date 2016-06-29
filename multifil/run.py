7cc5882da414b9f2f06b8119539998488e098ae6
Dave Williams <cdave@uw.edu>
2016-06-29 13:51:34 -0700

    Run emitter
    Can create and store JSON files describing what a run should do,
    including time varying z-line, ls, and actin activation

--- multifil/run.py
+++ multifil/run.py
@@ -10,6 +10,7 @@ import sys
 import os
 import time
 import uuid
+import ujson as json
 import pickle
 import shelve
 import copy
@@ -18,6 +19,7 @@ import multiprocessing as mp
 import boto
 import hs
 import numpy.random as random
+import numpy as np
 
 ## Settings
 BUCKET_NAME = 'model_results' # For results files
@@ -29,12 +31,158 @@ help_mesg = "Around here, we like to call our functions with a little \n \
       thing called options. Here's how we'd go about it: \n \
       usage: %prog [options] arg1 arg2 "
 
-## Logging
+## Helper functions
 def log_it(message):
     """Print message to sys.stdout"""
     sys.stdout.write("run.py " + mp.current_process().name + 
             " ## " + message + "\n")
     sys.stdout.flush()
+
+## Run emission
+def emit_run(path_local, path_s3, timestep_length, timestep_number, 
+             z_line=None, lattice_spacing=None, actin_permissiveness=None,
+             comment = None, write = True):
+    """Produce a structured JSON file that will be consumed to create a run
+    
+    emit_run is intended as the counterpoint to the run class and execute_run.
+    Import emit_run into an interactive workspace and populate a directory with
+    run configurations to be executed by a cluster of machines. 
+    
+    
+    Parameters
+    ----------
+    path_local: string
+        The local (absolute or relative) directory to which we save both 
+        emitted files and run output.
+    path_s3: string
+        The s3 bucket (and optional folder) to save run output to and to which
+        the emitted files should be uploaded.
+    timestep_length: float
+        Length of a timestep in ms
+    timestep_number: int
+        Number of timesteps run is simulated for
+    z_line: float or tuple, optional
+        If not given, default distance specified in hs.hs is used. If given as
+        float, the z-line distance for the run. If given as a tuple, used as 
+        arguments to the current z-line generation function within. The tuple
+        format is currently: (offset, amp, period)
+    lattice_spacing: float or tuple, optional
+        Same as for z-line. The tuple format is currently: (rest_ls, rest_zln)
+    actin_permissiveness: float or tuple, optional
+        Same as for z-line. The tuple format is currently: (amp, period, on, 
+        duration, sharp)
+    comment: string, optional
+        Space for comment on the purpose or other characteristics of the run
+    write: bool, optional
+        True (default) writes file to path_local/name.meta.json. Other values 
+        don't. In both cases the dictionary describing the run is returned.
+    
+    Returns
+    -------
+    rund: dict
+        Copy of run dictionary saved to disk as json.
+    
+    Examples
+    --------
+    >>> emit_run('./', None, .1, 100, write=False) 
+    {'actin_permissiveness': None,
+    ...  'actin_permissiveness_func': None,
+    ...  'comment': None,
+    ...  'lattice_spacing': None,
+    ...  'lattice_spacing_func': None,
+    ...  'name': ...,
+    ...  'path_local': './',
+    ...  'path_s3': None,
+    ...  'timestep_length': 0.1,
+    ...  'timestep_number': 100,
+    ...  'z_line': None,
+    ...  'z_line_func': None}
+    ...
+    >>> emit_run('./eggs', 'EGGS', 5, 10, (1250, 25, 25), (16, 1250), .75, comment='Just a test', write=False)  
+    {'actin_permissiveness': 0.75,
+    ... 'actin_permissiveness_func': None,
+    ... 'comment': 'Just a test',
+    ... 'lattice_spacing': array([ 16.        ,  15.92445392,  15.95318346,  16.04723114,
+    ... 16.07663156,  16.        ,  15.92445392,  15.95318346,
+    ... 16.04723114,  16.07663156]),
+    ... 'lattice_spacing_func': ("lambda rest_ls, rest_zln: np.sqrt(np.power(rest_ls,2) * rest_zln / rund['z_line'])",
+    ... (16, 1250)),
+    ... 'name': ...,
+    ... 'path_local': './eggs',
+    ... 'path_s3': 'EGGS',
+    ... 'timestep_length': 5,
+    ... 'timestep_number': 10,
+    ... 'z_line': array([ 1250.        ,  1261.88820645,  1257.34731565,  1242.65268435,
+    ... 1238.11179355,  1250.        ,  1261.88820645,  1257.34731565,
+    ... 1242.65268435,  1238.11179355]),
+    ... 'z_line_func': ('lambda offset, amp, period, time: offset + 0.5 * amp * np.sin(2*np.pi*time/period)',
+    ... (1250, 25, 25))}
+    ... 
+    """
+    rund = {}
+    ## Simple run metadata
+    name = str(uuid.uuid1())
+    rund['name'] = name
+    rund['comment'] = comment
+    rund['path_local'] = path_local
+    rund['path_s3'] = path_s3
+    rund['timestep_length'] = timestep_length
+    rund['timestep_number'] = timestep_number
+    ## Generate and store variable traces
+    time = np.arange(0, timestep_number*timestep_length, timestep_length)
+    # Define variable sarcomere length/z-line distance
+    def variable_z_line(offset, amp, period):
+        """Takes offset from zero, peak-to-peak amp, and period in ms"""
+        return offset + 0.5 * amp * np.sin(2*np.pi*time/period)
+    string_zln = "lambda offset, amp, period, time: offset + 0.5 * amp * np.sin(2*np.pi*time/period)"
+    # Parse sarcomere length
+    if hasattr(z_line, "__iter__"):
+        rund['z_line'] = variable_z_line(*z_line)
+        rund['z_line_func'] = (string_zln, z_line)
+    else:
+        rund['z_line'] = z_line
+        rund['z_line_func'] = None
+   # Define variable lattice spacing
+    def variable_lattice_spacing(rest_ls, rest_zln):
+        """Assumes constant volume relation, takes in rest ls and zline"""
+        return np.sqrt(np.power(rest_ls,2) * rest_zln / rund['z_line'])
+    string_ls = "lambda rest_ls, rest_zln: np.sqrt(np.power(rest_ls,2) * rest_zln / rund['z_line'])"
+    # Parse lattice spacing
+    if hasattr(lattice_spacing, "__iter__"):
+        rund['lattice_spacing'] = variable_lattice_spacing(*lattice_spacing)
+        rund['lattice_spacing_func'] = (string_ls, lattice_spacing)
+    else:
+        rund['lattice_spacing'] = lattice_spacing
+        rund['lattice_spacing_func'] = None
+    # Define variable actin permissiveness
+    def variable_actin_permissiveness(amp, period, on, duration, sharp):
+        """Requires amplitude, period of stim cycle, offset of turn-on from
+        start of a cycle, duration of on time, and sharpness (lower is more
+        gentle) of transition"""
+        sig = lambda on, x: \
+                0.5*amp*(np.tanh(sharp*(x-on))-np.tanh(sharp*(x-on-duration)))
+        cycle_number = int(time[-1])//period+1
+        return np.sum([sig(on+period*i, time) for i in range(cycle_number)], 0)
+    string_ap = """
+        sigmoid = lambda amp, on, duration, sharp, x: 
+            0.5*amp*(np.tanh(sharp*(x-on))-np.tanh(sharp*(x-on-duration)))
+        bumps = lambda amp, on, duration, sharp, period, x: 
+            np.sum([sigmoid(amp, on+period*i, duration, sharp, x) 
+            for i in range(int(x[-1])//period+1)],0) """
+    # Parse actin permissiveness 
+    if hasattr(actin_permissiveness, "__iter__"):
+        rund['actin_permissiveness'] = variable_actin_permissiveness(
+            *actin_permissiveness)
+        rund['actin_permissiveness_func'] = (string_ap, actin_permissiveness)
+    else:
+        rund['actin_permissiveness'] = actin_permissiveness
+        rund['actin_permissiveness_func'] = None
+    ## Write out the run description
+    if write is True:
+        output_filename = os.path.join(path_local, name+'.meta.json')
+        with open(output_filename , 'w') as metafile:
+            json.dump(rund, metafile, indent=4)
+    return rund
 
 ## Run status
 def run_status(i, total_steps, sec_left, sec_passed, process_name):
